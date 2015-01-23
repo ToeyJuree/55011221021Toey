d3a175c013720c1a4cdd52436742ca216448776d
iStudents <istudents@iClassrooms-iMac-32.local>
2015-01-23 16:38:37 +0700

    test_password lab4

--- test _password lab4.playground/section-1.swift
+++ test _password lab4.playground/section-1.swift
@@ -2,3 +2,26 @@
 
 import UIKit
 
+
+class password{
+    let Username: [String] = ["Panda", "Crocodile", "Parakeet"]
+    let password: [String] = ["1234", "M5678", "W91011"]
+    let user:String
+    
+    init(Username: String){
+        
+        self.user = Username
+    }
+    func UserPass() -> String {
+        var anser: String = ""
+        for(var i=0; i < Username.count ; i++){
+            if(Username[i] == user){
+                anser = password[i];
+            }
+        }
+        return anser
+    }
+    
+}
+let User1 = password(Username: "Panda")
+User1.UserPass()
