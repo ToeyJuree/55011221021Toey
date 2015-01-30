f3470ec62416a3a428b35d81018d54e44b054ee9
iStudents <istudents@iClassrooms-iMac-32.local>
2015-01-30 16:29:16 +0700

    week5_1

--- week5_1.playground/section-1.swift
+++ week5_1.playground/section-1.swift
@@ -2,5 +2,47 @@
 
 import UIKit
 
-class room {
+var str = "calculator"
+class calcul {
+    let n1: Double
+    let n2: Double
     
+    init(n1: Double, n2: Double){
+       self.n1 = n1
+       self.n2 = n2
+    }
+    func number(no:String){
+        if (n1>0 && n2<100){
+            var answer: Double
+            
+            if (no == "+"){
+              answer = n1+n2
+              println("\(n1) + \(n2) = \(answer)")
+                
+            }
+            else if (no == "-"){
+              answer = n1-n2
+              println("\(n1) - \(n2) = \(answer)")
+                
+            }
+            else if (no == "*"){
+              answer = n1*n2
+              println("\(n1) * \(n2) = \(answer)")
+                
+            }
+            else if (no == "/"){
+              answer = n1/n2
+              println("\(n1) / \(n2) = \(answer)")
+            }
+            
+        }
+        else{
+          
+            println("is n1-100")
+        }
+        
+        
+    }
+}
+let score = calcul(n1: 12, n2: 13)
+score.number("+")
